0425c63ad4881bd64c62a02be36343ff68dff756
AthanaseAlto <altondangang69@gmail.com>
2026-06-09 12:08:29 +0100

    logique du compte

---
+++ src/compte.py
@@ -0,0 +1,27 @@
+class Compte:
+    def init(self, client, solde_initial=0):
+        self.client = client
+        self.solde = solde_initial
+
+    def deposer(self, montant):
+        if montant > 0:
+            self.solde += montant
+            print(f"Dépôt de {montant} FCFA effectué.")
+        else:
+            print("Montant invalide.")
+
+    def retirer(self, montant):
+        if montant <= 0:
+            print("Montant invalide.")
+        elif montant > self.solde:
+            print("Solde insuffisant.")
+        else:
+            self.solde -= montant
+            print(f"Retrait de {montant} FCFA effectué.")
+
+    def str(self):
+        return (
+            f"\n=== COMPTE BANCAIRE ===\n"
+            f"Client : {self.client}\n"
+            f"Solde : {self.solde} FCFA"
+        )
